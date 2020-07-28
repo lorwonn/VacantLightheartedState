d113bf658f4836526155b34046ee469f42876b70
Jawon Blue <Jawonblue12@gmail.com>
2020-07-28 15:41:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+total-0
+def Start () :
+  a-str(input(Will you go first or second ))
+  if a == "first":
+    print (Player 1 starts
+    
+    def Add():
+      b==1:
+      total+1
+      print (total))
+      if b=="2
+      total+2
+      print (total)
+      print(Player 2 turn)
+start()
+while total=20
+add()
